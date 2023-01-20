9a3138f4520ad9630835f7cdb6d60d162d149452
josue4433 <113935346+josue4433@users.noreply.github.com>
2023-01-20 11:50:20 -0500

    Add files via upload

--- jeopardy.js
+++ jeopardy.js
@@ -1,21 +1,65 @@
-function startGame() {
-    const data = Object.assign({}, {category_ids:
-    cateogoryIDArray(totalNumberOfCateogories)})
+document.getElementById("submit-btn").addEventListener("click", onSubmit);
+document.getElementById("openUI").addEventListener("click", openUserInterface);
 
-    const configObject = {
-        method: "POST" ,
-        Headers: {
-            "Content-type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(data)
+function getRadioValue() {
+  var inputs = document.getElementsByName("gameType");
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].checked) {
+      return inputs[i].value;
     }
+  }
+};
 
-    fetch("http://localhost:3000/games", configObject)
-    .then(resp => resp.json())
-    .then(function(json) {
-        game = new Gamepad(json)
-        discardState()
-        renderBoard(game);
-    })
-}
+function onSubmit() {
+  var selections = ["Questions", "Answers"];
+  var selected = getRadioValue();
+  if (selected === "Questions") {
+    $(document).ready(function() {
+      $('td').click(function() {
+        $(this).text("question");
+        $(this).removeClass('.display-default');
+        $(this).removeClass('.display-answer');
+        $(this).addClass('.display-question');
+      });
+      $('td').dblclick(function() {
+        $(this).text("answer");
+        $(this).removeClass('.display-default');
+        $(this).removeClass('.display-question');
+        $(this).addClass('.display-answer');
+      });
+      $('#UserInterface').delay(100).hide();
+      $('#board').delay(100).show();
+    });
+  } else if (selected === "Answers") {
+    $(document).ready(function() {
+      $('#gameType-TXT1').css("color", "#000000");
+      $('#gameType-TXT2').css("color", "#000000");
+      $('td').click(function() {
+        $(this).text("answer");
+        $(this).removeClass('.display-default');
+        $(this).removeClass('.display-question');
+        $(this).addClass('.display-answer');
+      });
+      $('#gameType-TXT1').css("color", "#000000");
+      $('#gameType-TXT2').css("color", "#000000");
+      $('td').dblclick(function() {
+        $(this).text("question");
+        $(this).removeClass('.display-default');
+        $(this).removeClass('.display-answer');
+        $(this).addClass('.display-question');
+      });
+      $('#UserInterface').delay(100).hide();
+      $('#board').delay(100).show();
+    });
+  } else {
+    alert("ERROR: Unable to identify game type!\n\n TROUBLESHOOT: Select a game type.")
+    $('#gameType-TXT1').css("color", "#FF0000");
+    $('#gameType-TXT2').css("color", "#FF0000");
+    $('td','#UserInterface','#board').stop();
+  }
+};
+
+function openUserInterface() {
+  $('#UserInterface').delay(100).show();
+  $('#board').delay(100).hide();
+};
